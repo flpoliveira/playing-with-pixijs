24b3cb79c7b6ad006f76faf6f519639997812627
Filipe Oliveira <filipedasilvadeoliveira@gmail.com>
2024-07-20 13:31:59 -0300

    feat: initial repo

---
+++ webpack.config.prod.js
@@ -0,0 +1,34 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [],
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: "build/assets",
+        to: "assets",
+      },
+    ]),
+    new HTMLWebpackPlugin({
+      template: "build/index.html",
+      filename: "index.html",
+      hash: true,
+      minify: false,
+    }),
+  ],
+};
